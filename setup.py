c3a0df47cb577f5cf28a62455f4b60e04fbff120
Christophe Morisset <chris.morisset@gmail.com>
2019-09-02 16:56:21 -0700

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-#requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
-
 setup(
     name="tmniai",
-    version="0.0.1",
+    version="0.1.2",
     author="Christophe Morisset",
     author_email="Chris.Morisset@gmail.com",
     description="A package to developp AI tolls for Nebular studies",
@@ -15,7 +13,6 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/taller-mexicano-de-nebulosas-ionizadas/AI",
     packages=find_packages(),
-#    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
