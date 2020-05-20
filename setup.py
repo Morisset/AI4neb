1ad4964d61d5db914ac49861575a32cb3726c38c
Christophe Morisset <chris.morisset@gmail.com>
2020-05-20 08:33:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
-from mwinai.version import __version__
+from ai4neb.version import __version__
 
 setup(
-    name="mwinai",
+    name="ai4neb",
     version=__version__,
     author="Christophe Morisset",
     author_email="Chris.Morisset@gmail.com",
     description="A package to developp AI tools for Nebular studies",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/taller-mexicano-de-nebulosas-ionizadas/AI",
+    url="https://github.com/morisset/AI4neb",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
